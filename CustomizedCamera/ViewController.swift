8cd7346be51e9f5f8c57030d3cea925aa54ec3d4
garima94921 <gaarimabothra@gmail.com>
2020-05-03 15:29:36 +0530

    Camera View Added

--- CustomizedCamera/ViewController.swift
+++ CustomizedCamera/ViewController.swift
@@ -17,36 +17,43 @@ class ViewController: UIViewController {
     var frontCamera: AVCaptureDevice?
     var currentCamera: AVCaptureDevice?
     var photoOutput: AVCapturePhotoOutput?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
 
     @IBOutlet weak var previewView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+       // self.view.layer.backgroundColor = UIColor.white.withAlphaComponent(0.5) as! CGColor
         setupCaptureSession()
-        setupDevice()
-        setupInputOutput()
-        setupPreviewLayer()
-        startRunningCaptureSession()
+        setupDevice() { result in
+            if(result){
+                self.setupInputOutput()
+                self.setupPreviewLayer()
+                self.startRunningCaptureSession()
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
     func setupCaptureSession() {
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        print("Capture Setup")
     }
 
-    func setupDevice() {
+    func setupDevice(completion: @escaping(Bool) -> ()) {
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         let devices = deviceDiscoverySession.devices
-
         for device in devices {
             if device.position == AVCaptureDevice.Position.back {
                 backCamera = device
+                currentCamera = device
+                print(device)
             }
             else if device.position == AVCaptureDevice.Position.front {
                 frontCamera = device
             }
         }
-        currentCamera = backCamera
+        print("Device setup")
+        completion(true)
     }
 
     func setupInputOutput() {
@@ -57,14 +64,20 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
-
+        print("setupInputOutput")
     }
 
     func setupPreviewLayer() {
-
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer?.frame = self.view.frame
+        self.view.layer.insertSublayer(cameraPreviewLayer! , at: 0)
+        print("Setup preview layer")
     }
 
     func startRunningCaptureSession() {
+        captureSession.startRunning()
 
     }
 
