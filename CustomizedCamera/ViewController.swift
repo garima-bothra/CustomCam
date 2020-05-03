3d2a93400af1c20ce5b37aa0844c39f4a14c9cc4
garima94921 <gaarimabothra@gmail.com>
2020-05-03 14:52:23 +0530

    Basic Setup Done

--- CustomizedCamera/ViewController.swift
+++ CustomizedCamera/ViewController.swift
@@ -7,12 +7,73 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //Create variables
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    var photoOutput: AVCapturePhotoOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+    @IBOutlet weak var previewView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPreviewLayer()
+        startRunningCaptureSession()
         // Do any additional setup after loading the view.
+    }
+
+    func setupCaptureSession() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    }
+
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }
+            else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        currentCamera = backCamera
+    }
+
+    func setupInputOutput() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        } catch {
+            print(error)
+        }
+
+    }
+
+    func setupPreviewLayer() {
+
+    }
+
+    func startRunningCaptureSession() {
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       // Setup your camera here...
+    }
+
+    @IBAction func ClickImageButtonPressed(_ sender: Any) {
     }
 
 
